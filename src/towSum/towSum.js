db73e957f9e544c4927d576b3e134180b5bbc914
ivannissimrch <ivannissimrch@gmail.com>
2025-04-29 12:27:33 -0400

    Update tow sum

--- src/towSum/towSum.js
+++ src/towSum/towSum.js
@@ -4,33 +4,19 @@
  * @return {number[]}
  */
 const twoSum = function (nums, target) {
-  const sortedArray = nums.map((num, idx) => num);
-  console.log(sortedArray);
-  sortedArray.sort((a, b) => a - b);
-  console.log(sortedArray);
-
+  const sortedArray = nums.map((num, idx) => num).sort((a, b) => a - b);
   let leftIndex = 0;
   let rightIndex = sortedArray.length - 1;
+
   while (leftIndex < rightIndex) {
-    console.log("sorted array", sortedArray);
-    console.log("original array", nums);
-    console.log(
-      "left ad right index",
-      sortedArray[leftIndex],
-      sortedArray[rightIndex]
-    );
-
     const sum = sortedArray[leftIndex] + sortedArray[rightIndex];
-
     if (sum === target) {
       const result = [];
       nums.forEach((num, idx) => {
-        console.log("idx", idx);
         if (num === sortedArray[leftIndex] || num === sortedArray[rightIndex]) {
           result.push(idx);
         }
       });
-      console.log("result", result);
 
       return result;
     } else if (sum > target) {
