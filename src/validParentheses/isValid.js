1614da530dc2b4c247a8325d06a266a3d9e609c3
ivannissimrch <ivannissimrch@gmail.com>
2025-05-08 16:47:58 -0400

    update validParenteses solution2

--- src/validParentheses/isValid.js
+++ src/validParentheses/isValid.js
@@ -1,48 +1,76 @@
+//Solution 1 Working
+
+// function isValid(characters) {
+//   if (characters.length % 2 === 1) {
+//     return false;
+//   }
+
+//   const stack = [];
+//   let openParentheses = 0;
+//   let closingParentheses = 0;
+
+//   characters.split("").forEach((char) => {
+//     if (char === "(" || char === "[" || char === "{") {
+//       stack.push(char);
+//       openParentheses += 1;
+//     } else {
+//       closingParentheses += 1;
+//       if (char === ")" && stack[stack.length - 1] === "(") {
+//         stack.pop(stack[stack.length - 1]);
+//       }
+//       if (char === "]" && stack[stack.length - 1] === "[") {
+//         stack.pop(stack[stack.length - 1]);
+//       }
+//       if (char === "}" && stack[stack.length - 1] === "{") {
+//         stack.pop(stack[stack.length - 1]);
+//       }
+//     }
+//   });
+
+//   if (stack.length === 0 && openParentheses === closingParentheses) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+//solution 2
+
 function isValid(characters) {
   if (characters.length % 2 === 1) {
     return false;
   }
 
   const stack = [];
-  let openParentheses = 0;
-  let closingParentheses = 0;
+  const characterMap = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
 
-  // const characterMap = {
-  //   "(": ")",
-  //   "[": "]",
-  //   "{": "}",
-  // };
-
-  // const lastCharacter = [];
-
-  characters.split("").forEach((char) => {
-    if (char === "(" || char === "[" || char === "{") {
-      stack.push(char);
-      openParentheses += 1;
-    } else {
-      closingParentheses += 1;
-      if (char === ")" && stack[stack.length - 1] === "(") {
-        stack.pop(stack[stack.length - 1]);
-      }
-      if (char === "]" && stack[stack.length - 1] === "[") {
-        stack.pop(stack[stack.length - 1]);
-      }
-      if (char === "}" && stack[stack.length - 1] === "{") {
-        stack.pop(stack[stack.length - 1]);
-      }
+  for (let i = 0; i <= characters.length - 1; i++) {
+    console.log("1", stack);
+    if (
+      characters[i] === "(" ||
+      characters[i] === "[" ||
+      characters[i] === "{"
+    ) {
+      stack.push(characters[i]);
+      console.log("2", stack);
     }
-  });
-
-  console.log(openParentheses);
-  console.log(closingParentheses);
-  if (stack.length === 0 && openParentheses === closingParentheses) {
-    return true;
-  } else {
-    return false;
+    //also if array.length is 1
+    else if (characterMap[characters[i]] === stack[stack.length - 1]) {
+      stack.pop();
+      console.log("3", stack);
+    } else {
+      return false;
+    }
   }
+
+  return stack.length === 0;
 }
 
-console.log(isValid("()"));
-console.log(isValid("()[]{}"));
-console.log(isValid("([)]"));
+// console.log(isValid("()"));
+// console.log(isValid("()[]{}"));
+// console.log(isValid("([)]"));
 console.log(isValid("([}}])"));
