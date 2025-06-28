7e9909a846256c4a744b8131456cc7a969633ebe
ivannissimrch <ivannissimrch@gmail.com>
2025-06-28 05:07:26 -0400

    add neddle and romanToInt

--- src/needle.js
+++ src/needle.js
@@ -1,6 +1,4 @@
 function strStr(haystack, needle) {
-  let isFoundIndex = -1;
-  const haystackArray = haystack.split("");
   for (let i = 0; i <= haystack.length - needle.length; i++) {
     const substring = haystack.slice(i, i + needle.length);
     if (substring === needle) {
